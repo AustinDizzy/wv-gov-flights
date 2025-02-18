bd16cfa5e5251e5d80ab9a2cfc92e06b1819916b
Austin Siford <austin.siford@gmail.com>
2025-02-18 13:17:52 -0500

    Add note on statuses to Aircraft table

--- src/app/aircraft/aircraft-table.tsx
+++ src/app/aircraft/aircraft-table.tsx
@@ -12,12 +12,14 @@ import {
 import { Card } from "@/components/ui/card"
 import Link from "next/link"
 import { Badge } from "@/components/ui/badge"
+import { Info } from 'lucide-react'
 
 
 export function AircraftTable({ fleet }: {
     fleet: FleetMember[]
 }) {
     return (
+        <>
         <Card>
             <Table>
                 <TableHeader>
@@ -64,5 +66,11 @@ export function AircraftTable({ fleet }: {
                 </TableBody>
             </Table>
         </Card>
+        <div className="text-xs text-muted-foreground/50 flex items-center justify-end cursor-pointer mt-2">
+            <Info size={12} className="inline mr-1" />
+            N2WV was decommissioned in 2021.{" "}
+            Data collection for N890SP & N895SP is in-progress.
+        </div>
+        </>
     )
 }
