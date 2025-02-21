625ec293ef338f5d1c44ee21a07a6fb3ee7977cd
Austin Siford <austin.siford@gmail.com>
2025-02-21 13:15:15 -0500

    Updated footer styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,7 +8,7 @@ export const Footer: React.FC = () => {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 container mx-auto px-4 py-8">
                 <div className='text-muted-foreground text-xs space-y-4'>
                     <p>Information on this site is classified as public record and was obtained from legal requests pursuant to <Link href="https://code.wvlegislature.gov/29B-1/" className='border-b border-dotted border-[hsl(var(--muted-foreground))] whitespace-nowrap' target='nofollow noopener'>W.Va. Code § 29B-1-1 et. seq. (WVFOIA)</Link>. Read <Link href={"/data-sources"} className='hover:text-primary underline'>Data Sources</Link> for more information.</p>
-                    <p>This site is not affiliated with the government of the State of West Virginia.</p>
+                    <p>This site is not affiliated with the government of the <span className='whitespace-nowrap'>State of West Virginia</span>.</p>
                 </div>
                 <div className='ml-auto flex gap-8 items-center text-xs'>
                     <div className='flex flex-col gap-4 items-center ml-auto'>
